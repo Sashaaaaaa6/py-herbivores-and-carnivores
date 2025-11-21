c6b91ec5ac477192abec3909332c56365d6b8bd4
Sashaaaaaa6 <o.bulavin14@gmail.com>
2025-11-21 22:50:55 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            "{Name: " + self.name
+            + ", Health: " + str(self.health)
+            + ", Hidden: " + str(self.hidden) + "}"
+        )
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other: Animal) -> None:
+        if isinstance(other, Carnivore):
+            return
+        if other.hidden:
+            return
+        other.health -= 50
+        if other.health <= 0:
+            Animal.alive = [
+                a for a in Animal.alive if a is not other
+            ]
